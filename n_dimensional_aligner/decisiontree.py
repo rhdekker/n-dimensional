d67ce76ad04f8eab9c314a4a956e70e68afd7e3c
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2016-02-28 16:11:57 +0100

    DecisionTree: more ground work, introduced current_score, minimum_global_score and maximum_global_score.

--- n_dimensional_aligner/decisiontree.py
+++ n_dimensional_aligner/decisiontree.py
@@ -1,4 +1,5 @@
 # @author: Ronald Haentjens Dekker
+from collatex.tokenindex import TokenIndex
 
 
 def calculate_maximum(lcp_intervals):
@@ -15,8 +16,11 @@ def calculate_maximum(lcp_intervals):
 
 
 class DecisionNode(object):
-    def __init__(self):
+    def __init__(self, global_maximum_score):
         self.coordinates = (0, 0, 0)
+        self.current_score = 0
+        self.minimum_global_score = 0
+        self.maximum_global_score = global_maximum_score
     pass
 
 
@@ -24,6 +28,10 @@ class DecisionTree(object):
 
     def __init__(self, witnesses):
         self.witnesses = witnesses
-        self.root = DecisionNode()
+        self.tokenindex = TokenIndex(witnesses)
+        self.tokenindex.prepare()
+        self.lcp_intervals = self.tokenindex.split_lcp_array_into_intervals()
+        global_maximum_score = calculate_maximum(self.lcp_intervals)
+        self.root = DecisionNode(global_maximum_score)
 
     pass
--- n_dimensional_aligner/decisiontree_test.py
+++ n_dimensional_aligner/decisiontree_test.py
@@ -11,7 +11,7 @@ from n_dimensional_aligner.decisiontree import calculate_maximum, DecisionTree
 
 class DecisionTreeTest(TestCase):
 
-    def test_decision_tree(self):
+    def setUp(self):
         # we need to create witnesses
         # 1: a, b, c, d, e
         # 2: a, e, c, d
@@ -21,36 +21,33 @@ class DecisionTreeTest(TestCase):
         b = Witness({'id':'B', 'content':"a e c d"})
         c = Witness({'id':'C', 'content':"a d b"})
 
-        witnesses = [a, b, c]
-        tokenindex = TokenIndex(witnesses)
-        tokenindex.prepare()
-
-        tree = DecisionTree(witnesses)
-        root = tree.root
-        self.assertEquals((0, 0, 0), root.coordinates)
-
-
-
-
+        self.witnesses = [a, b, c]
+        self.tokenindex = TokenIndex(self.witnesses)
+        self.tokenindex.prepare()
 
     def test_maximum_score(self):
-        # we need to create witnesses
-        # 1: a, b, c, d, e
-        # 2: a, e, c, d
-        # 3: a, d, b
-
-        a = Witness({'id':'A', 'content':"a b c d e"})
-        b = Witness({'id':'B', 'content':"a e c d"})
-        c = Witness({'id':'C', 'content':"a d b"})
-
-        witnesses = [a, b, c]
-        tokenindex = TokenIndex(witnesses)
-        tokenindex.prepare()
-
         # from the token index we need to calculate the maximum amount of matches
-        lcp_intervals = tokenindex.split_lcp_array_into_intervals()
+        lcp_intervals = self.tokenindex.split_lcp_array_into_intervals()
         possible_matches = calculate_maximum(lcp_intervals)
 
         # print(possible_matches)
         self.assertEquals(12, possible_matches)
+
+    def test_decision_tree(self):
+        tree = DecisionTree(self.witnesses)
+        root = tree.root
+        self.assertEquals((0, 0, 0), root.coordinates)
+        # we need three scores, (current score), (minimum global score, maximum global score)
+        self.assertEquals(0, root.current_score)
+        self.assertEquals(0, root.minimum_global_score)
+        self.assertEquals(12, root.maximum_global_score)
+        # we need to keep track of the open options..
+        # we have the unexpanded ones, and the expanded ones..
+        # type of edit operation (attempt to match or to skip) and witness number the node represents (1, 2, 3)
+        # self.assertEquals()
+
+
+#    def
+
+
 
